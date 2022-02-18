020784a2c64175ab567ba2d71cc6fd0dba956b70
dudung <dudung@gmail.com>
2022-02-18 16:20:15 +0700

    edit and pause

--- app/ui/tabtncan/tabtncan.js
+++ app/ui/tabtncan/tabtncan.js
@@ -8,6 +8,10 @@
 	0937 Create main initVisualElement functions, empty.
 */
 
+// define global variables
+var div, divL, divR;
+var ta, btn, can;
+
 
 // call all necessary function
 function main() {
@@ -15,6 +19,28 @@ function main() {
 }
 
 
-// Initialize visual element
+// initialize visual element
 function initVisualElement() {
+	
+	div = document.createElement('div');
+	div.style.width = 400;
+	div.style.height = 300;
+	div.style.border = "1px solid #000";
+	div.style.background = "#eee";
+
+	divL = document.createElement('div');
+	divL.style.float = "left";
+	divL.style.width = 100;
+	divL.style.height = 300;
+	divL.style.background = "#fdd";
+
+	divR = document.createElement('div');
+	divR.style.float = "right";
+	divR.style.width = 300;
+	divR.style.height = 300;
+	divR.style.background = "#ddf";
+	
+	document.body.append(div);
+	div.append(divL);
+	div.append(divR);	
 }
