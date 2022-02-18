ae509f7050daf5d67a0df33395c3266e6dd520d7
dudung <dudung@gmail.com>
2022-02-18 09:38:47 +0700

    new html and js

---
+++ app/ui/tabtncan/tabtncan.js
@@ -0,0 +1,20 @@
+/*
+	tabtncan.js
+	A simple UI using textarea, button, and canvas
+	
+	Sparisoma Viridi | https://github.com/dudung/bugx-vis
+	
+	20220218 Start this example application.
+	0937 Create main initVisualElement functions, empty.
+*/
+
+
+// call all necessary function
+function main() {
+	initVisualElement();
+}
+
+
+// Initialize visual element
+function initVisualElement() {
+}
