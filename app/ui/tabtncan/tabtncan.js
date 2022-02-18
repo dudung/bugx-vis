e23235288811e766bdaa2cad0906a4e29ff2430a
dudung <dudung@gmail.com>
2022-02-18 18:23:37 +0700

    create two functions

--- app/ui/tabtncan/tabtncan.js
+++ app/ui/tabtncan/tabtncan.js
@@ -6,6 +6,7 @@
 	
 	20220218 Start this example application.
 	0937 Create main initVisualElement functions, empty.
+	1825 Make clearCanvas and drawCircle functions.
 */
 
 // define global variables
@@ -21,33 +22,55 @@ function main() {
 
 // initialize visual element
 function initVisualElement() {
-	
 	div = document.createElement('div');
 	div.style.width = 400;
-	div.style.height = 300;
-	div.style.border = "1px solid #000";
+	div.style.height = 250;
+	div.style.border = "0px solid #000";
 	div.style.background = "#eee";
 
 	divL = document.createElement('div');
 	divL.style.float = "left";
-	divL.style.width = 100;
-	divL.style.height = 300;
+	divL.style.width = 150;
+	divL.style.height = 250;
 	divL.style.background = "#fdd";
 	divL.style.border = "0px solid #000";
 
 	divR = document.createElement('div');
 	divR.style.float = "right";
-	divR.style.width = 300;
-	divR.style.height = 300;
+	divR.style.width = 250;
+	divR.style.height = 250;
 	divR.style.background = "#ddf";
 	
 	ta = document.createElement('textarea');
-	ta.style.width = 100;
-	ta.style.height = 250;
+	ta.style.width = 150;
+	ta.style.height = 228;
 	ta.style.overflowY = "scroll";
 
+	btn = document.createElement('button');
+	btn.innerHTML = "Start";
+	
+	can = document.createElement('canvas');
+	can.width = 250;
+	can.height = 250;
+	can.style.width = 250;
+	can.style.height = 250;
+	can.style.border = "1px solid #000";
+	
 	document.body.append(div);
 	div.append(divL);
 		divL.append(ta);
-	div.append(divR);	
+		divL.append(btn);
+	div.append(divR);
+		divR.append(can);
+}
+
+function clearCanvas() {
+	var ctx = can.getContext('2d');
+	ctx.clearRect(0, 0, can.width, can.height);
+}
+
+function drawCircle(x, y, r) {
+	var ctx = can.getContext('2d');
+	ctx.arc(x, y, r, 0, 2 * Math.PI)
+	ctx.stroke();
 }
